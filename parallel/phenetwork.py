2ae74af6785018e5959336e81276b29c94473277
Tian Zhou <yanxiangtianji@gmail.com>
2022-06-23 03:16:19 -0400

    feat: add function: in_shape and out_shape

--- parallel/phenetwork.py
+++ parallel/phenetwork.py
@@ -28,7 +28,24 @@ class PhenLayer():
         self.npart = nh*nw
         self.partid = hid*nw + wid
     
+    def __call__(self, x):
+        return self.forward(x)
+    
     def forward(self, x):
+        raise NotImplementedError("This function is not implemented")
+        
+    def in_shape(self):
+        """
+        Get the expected shape of input data, as a tuple.
+        For unfixed input: return None.
+        For dimensions with unfixed size: return None on that dimension.
+        """
+        raise NotImplementedError("This function is not implemented")
+        
+    def out_shape(self, inshape:tuple):
+        """
+        Get the expected shape of output data, as a tuple, given data as <inshape>.
+        """
         raise NotImplementedError("This function is not implemented")
     
 
@@ -42,6 +59,14 @@ class PhenConv(PhenLayer):
     
     def forward(self, x:np.ndarray):
         return computil.conv2d(x, self.conf, self.weight, self.bias)
+    
+    def in_shape(self):
+        return (self.in_ch, None, None)
+    
+    def out_shape(self, inshape:tuple):
+        assert len(inshape) == 3
+        assert inshape[0] == self.conf.out_ch
+        return self.conf.comp_out_size(inshape[1], inshape[2])
 
 
 class PhenLinear(PhenLayer):
@@ -49,8 +74,8 @@ class PhenLinear(PhenLayer):
         super().__init__(nh, nw, hid, wid)
         self.in_ch = linear.in_ch
         self.out_ch = linear.out_ch
-        self.weight = linear.weight
-        self.bias = linear.bias
+        self.weight = linear.weight # shape: out_ch * in_ch
+        self.bias = linear.bias # shape: out_ch
         # assume: self.in_ch % self.npart == 0
         m = self.in_ch // self.npart
         off_f = self.partid*m
@@ -59,8 +84,34 @@ class PhenLinear(PhenLayer):
         
     def forward(self, x:np.ndarray):
         #assert x.size == self.in_ch // self.npart
-        return heutil.dot_product_11(self.weight_slice, x)
+        return heutil.hewsum(x, self.weight_slice, self.bias)
+    #dot_product_11(self.weight_slice, x)
+
+    def in_shape(self):
+        return (self.in_ch, )
+
+    def out_shape(self, inshape:tuple):
+        assert len(inshape) == 1
+        return (self.out_ch, )
         
+
+class PhenFlatten(PhenLayer):
+    def __init__(self, nh, nw, hid, wid, ishape):
+        super().__init__(nh, nw, hid, wid)
+        self.ishape = ishape
+        assert ishape.ndim == 3
+        dch, dw, dh = ishape
+            
+    def forward(self, x:np.ndarray):
+        return x.reshape((-1))
+
+    def in_shape(self):
+        return None
+
+    def out_shape(self, inshape:tuple):
+        assert len(inshape) == 1
+        return (None, )
+
 
 class PhenRelu(PhenLayer):
     def __init__(self, nh, nw, hid, wid):
@@ -73,6 +124,12 @@ class PhenRelu(PhenLayer):
             shape = x.shape
             out = np.array([heutil.relu(i) for i in x.ravel()]).reshape(shape)
         return out
+
+    def in_shape(self):
+        return None
+
+    def out_shape(self, inshape:tuple):
+        return inshape
 
 
 class PhenSquare(PhenLayer):
@@ -87,6 +144,12 @@ class PhenSquare(PhenLayer):
             out = np.array([heutil.square(i) for i in x.ravel()]).reshape(shape)
         return out
 
+    def in_shape(self):
+        return None
+
+    def out_shape(self, inshape:tuple):
+        return inshape
+    
 
 # %% test
 
