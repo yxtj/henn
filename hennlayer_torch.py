1ee8ed8b9952b580b88bcf655aea1e8c45ca2604
Tian Zhou <yanxiangtianji@gmail.com>
2022-06-23 03:15:10 -0400

    feat: general bind

--- hennlayer_torch.py
+++ hennlayer_torch.py
@@ -5,7 +5,15 @@ import torch.nn as nn
 
 
 def bind_torch(he_module, torch_module):
-    pass
+    if isinstance(torch_module, nn.Linear):
+        he_module.weigth = torch_module.weight.detach().numpy()
+        he_module.bias = torch_module.bias.detach().numpy()
+    elif isinstance(torch_module, nn.Conv2d):
+        he_module.weigth = torch_module.weight.detach().numpy()
+        he_module.bias = torch_module.bias.detach().numpy()
+    else:
+        print("Not supported")
+    return he_module
 
 
 # %% linear
@@ -13,7 +21,7 @@ def bind_torch(he_module, torch_module):
 def make_linear(ref:nn.Linear):
     layer = hennlayer.Linear(ref.in_features, ref.out_features)
     layer.weight = ref.weight.data.detach().numpy()
-    if ref.bias:
+    if ref.bias is not None:
         layer.bias = ref.bias.detach().numpy()
     return layer
 
@@ -24,6 +32,6 @@ def make_conv2d(ref:nn.Conv2d):
     layer = hennlayer.Conv2d(ref.in_channels, ref.out_channels, ref.kernel_size,
                            ref.stride, ref.padding, ref.groups)
     layer.weight = ref.weight.data.detach().numpy()
-    if ref.bias:
+    if ref.bias is not None:
         layer.bias = ref.bias.detach().numpy()
     return layer
