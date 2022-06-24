764c33118b5a261cf428e4d2144a6632fa8bf779
Tian Zhou <yanxiangtianji@gmail.com>
2022-06-23 20:51:16 -0400

    test: module interface

--- test/__init__.py
+++ test/__init__.py
@@ -7,6 +7,15 @@ sys.path.append(SOURCE_PATH)
 
 
 if __name__ == "__main__":
-    import test_network
-    test_network.main()
-
+    if len(sys.argv) <= 1:
+        exit()
+    if sys.argv[1] == "network":
+        import test_network
+        test_network.main()
+    elif sys.argv[1] == "worker":
+        import test_worker
+        test_worker.main()
+    elif sys.argv[1] == "phen":
+        import test_phen
+        test_phen.main()
+        
