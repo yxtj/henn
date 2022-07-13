c3edb470ef4e86179d6d5e47ae935e4526e35428
Tian Zhou <yanxiangtianji@gmail.com>
2022-07-13 16:04:23 -0400

    chore: adapt worker with new PhenLayer APIs

--- parallel/worker.py
+++ parallel/worker.py
@@ -2,16 +2,15 @@
 
 import numpy as np
 import time
-import multiprocessing as mp
-
-#import computil
-from computil import Conv2dConf, conv2d
+#import multiprocessing as mp
 
 #import hennlayer
 
-from .data import PhenData
-from .phenetwork import PhenNetwork, PhenConv, PhenLinear, PhenRelu, PhenSquare
+from .data import load_data_part
+from .phenetwork import PhenLayer, PhenConv, PhenLinear, PhenFlatten, PhenRelu, PhenSquare
+from network import Network
 
+from .shaper import Shaper, make_shaper
 
 class Worker:
     def __init__(self, hid, wid, nh, nw):
@@ -20,64 +19,122 @@ class Worker:
         self.nh = nh # number of height separations
         self.nw = nw # number of width separations
         self.sid = hid*nw + wid # sequence id of this worker
-        # network related
+        # network
+        self.net = None
         self.worker_list = {} # map (hid, wid) to worker id
-        # data
-        self.data = None
         # neural network
         self.model = None
-    
+        self.datasize = []
+
     # interface functions
-    
+
     def init_network(self):
-        pass
-    
-    def load_data(self, data:np.ndarray):
-        pass
-    
-    def load_model(self, net:PhenNetwork):
-        pass
-    
-    def run(self):
-        pass
-    
+        self.net = Network()
+        r = self.net.alltoall((self.hid, self.wid))
+        for i, c in enumerate(r):
+            self.worker_list[c] = i
+        print(f"Worker {self.net.rank} registered in network.")
+
+    def init_model(self, model:list[PhenLayer], inshape:tuple):
+        self.model = model
+        #self.planner = Planner(self.nh, self.nw, model, inshape)
+        self.gshapes = []
+        self.shapers = []
+        self.ltypes = []
+        s = inshape
+        self.gshapes.append(s)
+        self.shapers.append(make_shaper(self.nh, self.nw, len(s), s))
+        for idx, layer in enumerate(self.model):
+            s = layer.out_shape(s)
+            ss = make_shaper(self.nh, self.nw, layer.dim, s)
+            self.gshapes.append(s)
+            self.shapers.append(ss)
+            self.ltypes.append(layer.ltype)
+        for idx in range(len(self.model)):
+            layer.bind_in_model(self.shapers[idx], self.shapers[idx+1],
+                                idx, self.gshapes[idx], self.ltypes[idx])
+
+    def run(self, data:np.ndarray):
+        x = load_data_part(data)
+        for layer in self.model:
+            if isinstance(layer, PhenConv):
+                x = self.comp_conv(x, layer)
+            elif isinstance(layer, PhenConv):
+                x = self.comp_linear(x, layer)
+            elif isinstance(layer, PhenFlatten):
+                x = self.comp_flatten(x)
+            elif isinstance(layer, PhenRelu) or isinstance(layer, PhenSquare):
+                x = self.comp_act(x)
+            else:
+                print(f"{layer.layer_idx}-th layer of type {type(layer)}"
+                      " is not supported")
+        return x
+
     # inner functions - compute
-    
-    def comp_conv(self, data, conv:PhenConv):
-        pass
-    
-    def comp_linear(self, data):
-        pass
-    
-    def comp_act(self, data):
-        pass
-    
+
+    def comp_conv(self, x, conv:PhenConv):
+        x = self.preprocess(conv, x)
+        x = conv.forward(x)
+        x = self.postprocess(conv, x)
+        return x
+
+    def comp_linear(self, x, fc:PhenLinear):
+        x = self.preprocess(fc, x)
+        x = fc.forward(x)
+        x = self.postprocess(fc, x)
+        return x
+
+    def comp_flatten(self, x, fn:PhenFlatten):
+        x = fn.forward(x)
+        return x
+
+    def comp_act(self, x, act:PhenRelu):
+        x = act.forward(x)
+        return x
+
     # inner functions - data
 
-    def wait_joint_data(self):
-        pass
-    
-    def send_joint_data(self, off_h, off_w, data):
-        assert off_h in (0, -1)
-        assert off_w in (0, -1)
-        assert off_h == -1 or off_w == -1
-        tgt_hid = self.hid + off_h
-        tgt_wid = self.wid + off_w
-        
-        pass
-    
-    def join_data(self):
-        pass
-        
+    def preprocess(self, layer:PhenLayer, x:np.ndarray):
+        # prepare data depdency
+        rqtgts = layer.depend_out(x)
+        for hid, wid, desc in rqtgts:
+            msg = layer.depend_message(x, hid, wid, desc)
+            self.send(hid, wid, msg)
+        xlist = []
+        n = len(layer.depend_in())
+        for i in range(n):
+            d = self.recv()
+            xlist.append(d)
+        x = layer.depend_merge(x, xlist)
+        # padding
+        x = layer.local_prepare(x)
+        return x
+
+    def postprocess(self, layer:PhenLayer, x:np.ndarray):
+        # load balance
+        tgts = layer.join_out()
+        for hid, wid, desc in tgts:
+            msg = layer.join_message(x, hid, wid, desc)
+            self.send(hid, wid, msg)
+        n = len(layer.join_in())
+        #if n == 0:
+        xlist = []
+        for i in range(n):
+            d = self. recv()
+            xlist.append(d)
+        x = layer.join_merge(x, xlist)
+        return x
+
     # low level functions
-    
-    def net_send(self, tgt_worker_id, data):
-        pass
-    
-    def net_recv(self):
-        pass
-    
-    
-    
-    
-    
+
+    def send(self, hid, wid, data):
+        worker_id = hid*self.nw + wid
+        self.net.isend(data, worker_id)
+
+    def recv(self):
+        return self.net.recv()
+
+
+
+
+
