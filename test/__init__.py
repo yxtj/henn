891c4397b6630fd4c169d8911764d84200c3c971
Tian Zhou <yanxiangtianji@gmail.com>
2022-08-04 18:01:22 -0400

    test: add new unit tests into the module

--- test/__init__.py
+++ test/__init__.py
@@ -9,7 +9,10 @@ sys.path.append(SOURCE_PATH)
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
         exit()
-    if sys.argv[1] == "network":
+    if sys.argv[1] == "pl_basis":
+        import test_pl_basis
+        test_pl_basis.main()
+    elif sys.argv[1] == "network":
         import test_network
         test_network.main()
     elif sys.argv[1] == "worker":
@@ -18,4 +21,8 @@ if __name__ == "__main__":
     elif sys.argv[1] == "phen":
         import test_phen
         test_phen.main()
-        
+    elif sys.argv[1] == "nnmodel":
+        import test_nnmodel
+        test_nnmodel.main()
+    else:
+        print(sys.argv[1], "not supported")
