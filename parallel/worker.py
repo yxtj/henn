5f2b28a4fecbf3495e2ce886d59a90534ae4ca08
Tian Zhou <yanxiangtianji@gmail.com>
2022-08-03 23:54:37 -0400

    fix: worker class

--- parallel/worker.py
+++ parallel/worker.py
@@ -2,15 +2,15 @@
 
 import numpy as np
 import time
+import sys
 #import multiprocessing as mp
 
 #import hennlayer
 
-from .data import load_data_part
 from .phenetwork import PhenLayer, PhenConv, PhenLinear, PhenFlatten, PhenRelu, PhenSquare
 from network import Network
 
-from .shaper import Shaper, make_shaper
+from .shaper import make_shaper
 
 class Worker:
     def __init__(self, hid, wid, nh, nw):
@@ -24,7 +24,23 @@ class Worker:
         self.worker_list = {} # map (hid, wid) to worker id
         # neural network
         self.model = None
-        self.datasize = []
+        self.gshapes = None
+        self.shapers = None
+        self.ltypes = None
+        # statistic - network
+        self.stat_send_msg = 0
+        self.stat_send_byte = 0
+        self.stat_recv_msg = 0
+        self.stat_recv_byte = 0
+        # statistic - computation
+        self.stat_time_layer_prepare = []
+        self.stat_time_layer_compute = []
+        self.stat_time_layer_postprocess = []
+        self.stat_time_layer = []
+        self.stat_time_sync = 0.0
+        self.stat_time_send = 0.0
+        self.stat_time_recv = 0.0
+
 
     # interface functions
 
@@ -33,63 +49,121 @@ class Worker:
         r = self.net.alltoall((self.hid, self.wid))
         for i, c in enumerate(r):
             self.worker_list[c] = i
-        print(f"Worker {self.net.rank} registered in network.")
+        print(f"Worker {self.hid}-{self.wid} registered in network.")
 
     def init_model(self, model:list[PhenLayer], inshape:tuple):
         self.model = model
-        #self.planner = Planner(self.nh, self.nw, model, inshape)
-        self.gshapes = []
-        self.shapers = []
-        self.ltypes = []
-        s = inshape
-        self.gshapes.append(s)
-        self.shapers.append(make_shaper(self.nh, self.nw, len(s), s))
-        for idx, layer in enumerate(self.model):
-            s = layer.out_shape(s)
-            ss = make_shaper(self.nh, self.nw, layer.dim, s)
-            self.gshapes.append(s)
-            self.shapers.append(ss)
-            self.ltypes.append(layer.ltype)
-        for idx in range(len(self.model)):
-            layer.bind_in_model(self.shapers[idx], self.shapers[idx+1],
-                                idx, self.gshapes[idx], self.ltypes[idx])
+        gshapes, shapers, layer_types = self.compute_model_meta( model, inshape)
+        self.gshapes = gshapes
+        self.shapers = shapers
+        self.ltypes = layer_types
+        n = len(model)
+        for lid in range(n):
+            model[lid].bind_in_model(shapers[lid], shapers[lid+1],
+                                     lid, gshapes, layer_types)
+        self.stat_time_layer_prepare = [0.0 for _ in range(n)]
+        self.stat_time_layer_compute = [0.0 for _ in range(n)]
+        self.stat_time_layer_postprocess = [0.0 for _ in range(n)]
+        self.stat_time_layer = [0.0 for _ in range(n)]
+        print(f"Worker {self.hid}-{self.wid} load model.")
 
-    def run(self, data:np.ndarray):
-        x = load_data_part(data)
-        for layer in self.model:
+    def run(self, data:np.ndarray, complete_input=True):
+        if complete_input == True:
+            s = self.shapers[0]
+            x = s.pick_data(self.hid, self.wid, data)
+        else:
+            x = data
+        for lid, layer in enumerate(self.model):
+            t = time.time()
             if isinstance(layer, PhenConv):
-                x = self.comp_conv(x, layer)
-            elif isinstance(layer, PhenConv):
-                x = self.comp_linear(x, layer)
+                x = self.comp_conv(x, lid, layer)
+            elif isinstance(layer, PhenLinear):
+                x = self.comp_linear(x, lid, layer)
             elif isinstance(layer, PhenFlatten):
-                x = self.comp_flatten(x)
+                x = self.comp_flatten(x, lid, layer)
             elif isinstance(layer, PhenRelu) or isinstance(layer, PhenSquare):
-                x = self.comp_act(x)
+                x = self.comp_act(x, lid, layer)
             else:
-                print(f"{layer.layer_idx}-th layer of type {type(layer)}"
+                print(f"{lid}-th layer of type {self.ltypes[lid]}"
                       " is not supported")
+            t = time.time() - t
+            self.stat_time_layer[lid] += t
         return x
+
+    def show_stat(self):
+        s = f"Statistics: worker {self.hid}-{self.wid} (w{self.sid}):\n"\
+            f"  send {self.stat_send_msg} messages, {self.stat_send_byte} bytes; "\
+            f"recv {self.stat_recv_msg} messages, {self.stat_recv_byte} bytes;\n"\
+            f"  layer total time: {self.stat_time_layer};\n"\
+            f"    prepare time: {self.stat_time_layer_prepare};\n"\
+            f"    compute time: {self.stat_time_layer_compute};\n"\
+            f"    postprocess time: {self.stat_time_layer_postprocess};\n"\
+            f"  synchronization time: {self.stat_time_sync}, "\
+            f"send time: {self.stat_time_send}, recv time: {self.stat_time_recv}"
+        print(s)
+
+    # inner functions - model prepare
+
+    def compute_model_meta(self, model:list[PhenLayer], inshape:tuple):
+        gshapes = [ inshape ]
+        shapers = [ make_shaper(self.nh, self.nw, min(2, len(inshape)), inshape) ]
+        layer_types = []
+        s = inshape
+        for lyr in model:
+            print(s, lyr)
+            s = lyr.out_shape(s)
+            gshapes.append(s)
+            t = lyr.ltype
+            layer_types.append(t)
+            if t == "conv":
+                ss = make_shaper(self.nh, self.nw, 2, s)
+            elif t == 'linear':
+                ss = make_shaper(self.nh, self.nw, 1, s)
+            elif t == 'flatten':
+                ss = make_shaper(self.nh, self.nw, 1, s)
+            shapers.append(ss)
+        return gshapes, shapers, layer_types
 
     # inner functions - compute
 
-    def comp_conv(self, x, conv:PhenConv):
+    def comp_conv(self, x, lid:int, conv:PhenConv):
+        t0 = time.time()
         x = self.preprocess(conv, x)
-        x = conv.forward(x)
+        t1 = time.time()
+        x = conv.local_forward(x)
+        t2 = time.time()
         x = self.postprocess(conv, x)
+        t3 = time.time()
+        self.stat_time_layer_prepare[lid] = t1-t0
+        self.stat_time_layer_compute[lid] = t2-t1
+        self.stat_time_layer_postprocess[lid] = t3-t2
         return x
 
-    def comp_linear(self, x, fc:PhenLinear):
+    def comp_linear(self, x, lid:int, fc:PhenLinear):
+        t0 = time.time()
         x = self.preprocess(fc, x)
-        x = fc.forward(x)
+        t1 = time.time()
+        x = fc.local_forward(x)
+        t2 = time.time()
         x = self.postprocess(fc, x)
+        t3 = time.time()
+        self.stat_time_layer_prepare[lid] = t1-t0
+        self.stat_time_layer_compute[lid] = t2-t1
+        self.stat_time_layer_postprocess[lid] = t3-t2
         return x
 
-    def comp_flatten(self, x, fn:PhenFlatten):
-        x = fn.forward(x)
+    def comp_flatten(self, x, lid:int, fn:PhenFlatten):
+        t = time.time()
+        x = fn.local_forward(x)
+        t = time.time() - t
+        self.stat_time_layer_compute[lid] = t
         return x
 
-    def comp_act(self, x, act:PhenRelu):
-        x = act.forward(x)
+    def comp_act(self, x, lid:int, act:PhenRelu):
+        t = time.time()
+        x = act.local_forward(x)
+        t = time.time() - t
+        self.stat_time_layer_compute[lid] = t
         return x
 
     # inner functions - data
@@ -101,26 +175,27 @@ class Worker:
             msg = layer.depend_message(x, hid, wid, desc)
             self.send(hid, wid, msg)
         xlist = []
-        n = len(layer.depend_in())
-        for i in range(n):
+        dep = layer.depend_in(x)
+        for i in range(len(dep)):
             d = self.recv()
             xlist.append(d)
+        dep_req = [(h, w) for h, w, d in dep]
+        dep_rec = [(h, w) for h, w, d in xlist]
+        assert sorted(dep_req) == sorted(dep_rec), f"req:{dep_req}, recv:{dep_rec}"
         x = layer.depend_merge(x, xlist)
-        # padding
-        x = layer.local_prepare(x)
         return x
 
     def postprocess(self, layer:PhenLayer, x:np.ndarray):
         # load balance
-        tgts = layer.join_out()
+        tgts = layer.join_out(x)
         for hid, wid, desc in tgts:
             msg = layer.join_message(x, hid, wid, desc)
             self.send(hid, wid, msg)
-        n = len(layer.join_in())
+        req = layer.join_in(x)
         #if n == 0:
         xlist = []
-        for i in range(n):
-            d = self. recv()
+        for i in range(len(req)):
+            d = self.recv()
             xlist.append(d)
         x = layer.join_merge(x, xlist)
         return x
@@ -128,12 +203,29 @@ class Worker:
     # low level functions
 
     def send(self, hid, wid, data):
+        t = time.time()
         worker_id = hid*self.nw + wid
+        self.stat_send_msg += 1
+        self.stat_send_byte += sys.getsizeof(data)
         self.net.isend(data, worker_id)
+        t = time.time() - t
+        self.stat_time_send += t
+
 
     def recv(self):
-        return self.net.recv()
+        t = time.time()
+        data = self.net.recv()
+        self.stat_recv_msg += 1
+        self.stat_recv_byte += sys.getsizeof(data)
+        t = time.time() - t
+        self.stat_time_recv += t
+        return data
 
+    def sync(self):
+        t = time.time()
+        self.net.barrier()
+        t = time.time() - t
+        self.stat_time_sync += t
 
 
 
